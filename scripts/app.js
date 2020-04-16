854752bcddd0dfef62160004654ddf81d112259f
joanawang <jomary.wang@gmail.com>
2020-04-16 15:59:20 +0100

    Phone over grid absolute

--- scripts/app.js
+++ scripts/app.js
@@ -35,7 +35,7 @@ function init() {
     function createGrid(startingPosition) {
         for (let i = 0; i < cellCount; i++) {
             const cell = document.createElement('div')
-            cell.textContent = i
+                //cell.textContent = i
             grid.appendChild(cell)
             cells.push(cell)
         }
